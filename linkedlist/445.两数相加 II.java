54ef33c5afe9bf2e78104ce8f64c7e75d9402ffd
senjougahara77 <58139851+senjougahara77@users.noreply.github.com>
2021-05-18 16:43:02 +0800

    Create 445.两数相加 II.java

---
+++ linkedlist/445.两数相加 II.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Deque<Integer> stack1 = new ArrayDeque<>();
+        Deque<Integer> stack2 = new ArrayDeque<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        ListNode dummy = null;
+        int carry = 0;
+        int val = 0;
+        while (!stack1.isEmpty() || !stack2.isEmpty()) {
+            int a = stack1.isEmpty() ? 0 : stack1.pop();
+            int b = stack2.isEmpty() ? 0 : stack2.pop();
+            carry = a + b + carry;
+            val = carry % 10;
+            carry /= 10;
+            ListNode cur = new ListNode(val);
+            cur.next = dummy;
+            dummy = cur;
+        }
+        if (carry == 1) {
+            ListNode cur = new ListNode(1);
+            cur.next = dummy;
+            dummy = cur;
+        }
+        return dummy;
+    }
+}
