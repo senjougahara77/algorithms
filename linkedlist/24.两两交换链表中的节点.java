351679d3ab4c8d765579147785838874f5975066
senjougahara77 <58139851+senjougahara77@users.noreply.github.com>
2021-05-18 14:49:04 +0800

    Create 24.两两交换链表中的节点.java

---
+++ linkedlist/24.两两交换链表中的节点.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode a = head;
+        ListNode b = head.next;
+        a.next = swapPairs(b.next);
+        b.next = a;
+        return b;
+    }
+}
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            ListNode node1 = cur.next;
+            ListNode node2 = cur.next.next;
+            cur.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            cur = node1;
+        }
+        return dummy.next;
+    }
+}
